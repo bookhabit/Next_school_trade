0586b832fac5963d8d8178a638194ec4dbda7f39
bookhabit <qnrgoqlt@naver.com>
2023-01-23 00:47:16 +0900

    스타일링 설정 글로벌 스타일 폰트설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container  = styled.div`
+    font-size:21px;
+    color:black;
+`
 
 const index:React.FC = ()=>{
-    return <div>hello world</div>;
+    return (
+    <Container>
+        <h1>hello </h1>
+        <ul>
+            <li>hi li</li>
+        </ul>
+        <a>hi a</a>
+    </Container>);
 }
 
 export default index;
