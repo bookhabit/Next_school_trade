7e0fcc0fb924da7eda4a192bac914c71eb954db2
bookhabit <qnrgoqlt@naver.com>
2023-03-03 14:51:30 +0900

    subHeader컴포넌트 currentLeft에 따라 UI변경

--- components/header/SubHeader.tsx
+++ components/header/SubHeader.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import palette from '../../styles/palette';
+import { useState, useRef } from 'react';
 
-const Container = styled.div`
+const Container = styled.div<{currentLeft:boolean}>`
     width:100%;
     height:50px;
     display:flex;
-    background-color:#D9D9D9;
     p{
         font-size:20px;
         font-weight:bold;
@@ -17,6 +17,8 @@ const Container = styled.div`
         display:flex;
         justify-content:space-evenly;
         align-items:center;
+        cursor: pointer;
+        background-color:${props=>props.currentLeft? "#FFFFFF" :"#D9D9D9"}
     }
     .subHeader-right{
         width:195px;
@@ -24,6 +26,8 @@ const Container = styled.div`
         display:flex;
         justify-content:space-evenly;
         align-items:center;
+        cursor: pointer;
+        background-color:${props=>props.currentLeft?"#D9D9D9" :"#FFFFFF" }
     }
     .active{
         background-color:white;
@@ -31,12 +35,18 @@ const Container = styled.div`
 `
 
 const SubHeader = () => {
+    // currentLeft에 따라서 UI가 바뀌고 다른 정보를 가져오면 된다
+    const [currentLeft,setCurrentLeft] = useState(true);
+    const changeCurrent = ()=>{
+        setCurrentLeft(!currentLeft)
+    }
+    console.log(currentLeft)
     return (
-        <Container>
-            <div className='subHeader-left active'>
+        <Container currentLeft={currentLeft}>
+            <div className='subHeader-left' onClick={changeCurrent}>
                 <p>로그인</p>
             </div>
-            <div className='subHeader-right'>
+            <div className='subHeader-right' onClick={changeCurrent}>
                 <p>회원가입</p>
             </div>
         </Container>
