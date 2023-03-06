8c27d997538e8048c4e4f31374b47c9222d208cb
bookhabit <qnrgoqlt@naver.com>
2023-03-06 13:43:07 +0900

    LinkFooter 컴포넌트수정

--- components/footer/LinkFooter.tsx
+++ components/footer/LinkFooter.tsx
@@ -2,72 +2,62 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import palette from '../../styles/palette';
-import LinkModal from './LinkModal';
+import HomeIcon from "../../public/static/svg/footer/home.svg"
+import RegisterIcon from "../../public/static/svg/footer/register.svg"
+import UserIcon from "../../public/static/svg/footer/user.svg"
+import ChattingIcon from "../../public/static/svg/footer/chatting.svg"
 
 const Container = styled.div`
     position:sticky;
     position:absolute;
-    bottom:0px;        
-    right:0px;
-
-    .linkModal{
-        background-color:${palette.main_color};
-        border-radius:20px;
-        width:140px;
-        height:175px;
+    bottom:0px;      
+    background-color:${palette.main_color};
+    width:100%;
+    height:60px;
+    
+    .link-flex-wrap{
         display:flex;
-        flex-direction:column;
-        justify-content:center;
-        align-items:center;
-        margin-right:25px;
-        p{
-            color:${palette.main_text_color};
-            font-size:20px;
-            font-weight:700;
-            cursor: pointer;
-            margin-top:10px;
-            margin-bottom:10px;
-        }
-        p:hover{
-            color:${palette.text_hover}
+        justify-content:space-evenly;
+        a{
+            display:flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
+            padding:10px;
         }
 
-    }
-    .linkFooter{
-        text-align:right;
-        margin-top:5px;
-        margin-right:25px;
-        margin-bottom:30px;
-        button{
-            width:60px;
-            height:60px;
-            background-color:${palette.main_color};
-            border-radius:50px;
-            color:${palette.main_text_color};
-            font-size:30px;
+        p{
+            margin-top:5px;
+            font-size:13px;
             font-weight:bold;
-            cursor: pointer;
+            color:${palette.main_text_color}
         }
-        button:hover{
-            background-color:${palette.btn_hover}
-        }
-        
     }
+    
 
 `
 
 const LinkFooter = () => {
-    const [showLinkModal,setShowLinkModal] = useState(false)
-
-    const toggleModal = ()=>{
-        setShowLinkModal(!showLinkModal)
-    }
-
     return (
         <Container>
-            {showLinkModal?<LinkModal/>:null}
-            <div className='linkFooter'>
-                <button onClick={toggleModal}>+</button>
+            <div className='link-flex-wrap'>
+                <Link href="/home">
+                    <HomeIcon/>
+                    <p>홈</p>
+                </Link>
+                <Link href="/registerProduct">
+                    <RegisterIcon/>
+                    <p>등록</p>
+                </Link>
+                <Link href="/user/chattingList">
+                    <ChattingIcon/>
+                    <p>채팅</p>
+                </Link>
+                <Link href="/user/myPage">
+                    <UserIcon/>
+                    <p>내정보</p>
+                </Link>
+
             </div>
         </Container>
     );
