57785a98eba17fd10988fda4f5db2a3d78134512
bookhabit <qnrgoqlt@naver.com>
2023-03-13 23:49:53 +0900

    sweetalert2로 알림창 설정 - 거래위치설정

--- components/map/setPosition.tsx
+++ components/map/setPosition.tsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 import throttle from "lodash/throttle";
 import { useDispatch } from 'react-redux';
 import { registerPositionActions } from './../../store/registerPosition';
+import Swal from 'sweetalert2';
 
 const Container = styled.div`
     .mordal-close-x-icon {
@@ -45,13 +46,13 @@ const Container = styled.div`
 
     .set-position-current-location{
         background-color:${palette.main_color};
-        color:${palette.main_text_color};
         height:20px;
         border-bottom:1px solid ${palette.divistion_color};
-        p{
-            padding-top:2px;
+        button{
+            padding-bottom:1px;
+            color:${palette.main_text_color};
             width:100%;
-            font-size:15px;
+            font-size:14px;
             text-align:center;
         }
     }
@@ -164,6 +165,7 @@ const SetPosition:React.FC<IProps> = ({closeModal}) => {
 
 
     const initMap = ()=>{
+        console.log('지도불러온 횟수 - initMap')
         // 지도 불러오기
         if(mapRef.current){
             const map:any = new window.google.maps.Map(mapRef.current,{
@@ -171,7 +173,7 @@ const SetPosition:React.FC<IProps> = ({closeModal}) => {
                     lat:currentLocation.latitude,
                     lng:currentLocation.longitude
                 },
-                zoom:18,
+                zoom:16,
             });
             const marker = new window.google.maps.Marker({
                 position:{
@@ -194,17 +196,44 @@ const SetPosition:React.FC<IProps> = ({closeModal}) => {
         }
     }
 
-    useEffect(()=>{
-        loadMap();
-        window.initMap = initMap
-    },[])
+    // useEffect(()=>{
+    //     console.log('지도불러온 횟수 - useEffect')
+    //     loadMap();
+    //     window.initMap = initMap
+    // },[])
+
+    const setCurrentPosition = ()=>{
+        console.log('현재 위치설정')
+        Swal.fire('현재 위치를 설정하였습니다.')
+    }
 
     // 주 거래 위치로 설정하기  - 위치,위도,경도를 registerPositon 리덕스 스토어에 저장한다
     const savePosition = ()=>{
         dispatch(registerPositionActions.setLatitude(currentLocation.latitude));
         dispatch(registerPositionActions.setLongitude(currentLocation.longitude));
         dispatch(registerPositionActions.setLocation(inputLocation));
-        closeModal();
+        // sweetAlert로 알림창 꾸미기
+        Swal.fire({
+            title: '거래 위치를 설정하시겠습니까?',
+            text: '다시 되돌릴 수 없습니다.',
+            icon: 'warning',
+            
+            showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+            confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+            cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+            confirmButtonText: '승인', // confirm 버튼 텍스트 지정
+            cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+            
+            reverseButtons: true, // 버튼 순서 거꾸로
+            
+         }).then(result => {
+            // 만약 Promise리턴을 받으면,
+            if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+            
+               Swal.fire('거래 위치가 설정되었습니다.', 'success');
+            }
+         });
+        // closeModal();
     }
     
     return (
@@ -217,7 +246,7 @@ const SetPosition:React.FC<IProps> = ({closeModal}) => {
                 <MarkerIcon/>
             </div>
             <div className='set-position-current-location'>
-                <p>현재위치로 설정하기</p>
+                <button onClick={setCurrentPosition}>현재위치로 설정하기</button>
             </div>
             <div className='set-position-name'>
                 <p>선택한 곳의 장소명을 입력해주세요</p>
