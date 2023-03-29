ac9b168e5eab6123e1824e122dc695d19da6d436
bookhabit <qnrgoqlt@naver.com>
2023-03-29 15:05:12 +0900

    서버사이드렌더링으로 인피니티쿼리 적용

--- pages/home/[name].tsx
+++ pages/home/[name].tsx
@@ -1,34 +1,92 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import LinkFooter from '../../components/footer/LinkFooter';
-import Home from "../../components/home/Home";
 import { getCategoryProductList, GetproductList } from '../../lib/api/product';
+import styled from 'styled-components';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
+import ProductList from '../../components/home/ProductList';
+import { dehydrate, QueryClient, useInfiniteQuery } from '@tanstack/react-query';
 
-const categoryHome = ({data}:any) => {
-    console.log('categoryHome',data)
+
+const Container = styled.div`
+    padding:0px 20px;
+    padding-top:20px;
+`
+const categoryHome = ({categoryName}:any) => {
+    console.log(categoryName)
+    const lastPageNumber=1 // 백엔드 offset 받아와야함
+    const {
+        data, // 💡 data.pages를 갖고 있는 배열
+        fetchNextPage, // 💡 다음 페이지를 불러오는 함수
+        hasNextPage, // 다음 페이지가 있는지 여부, Boolean
+        status, 
+    } = useInfiniteQuery(
+          ["categoryList"] 
+        , async ()=> await getCategoryProductList(categoryName)
+        , {
+            // 위의 fetch callback의 인자로 자동으로 pageParam을 전달.
+            getNextPageParam: (_lastPage,pages) => {
+                if(pages.length<lastPageNumber){
+                    return pages.length
+                }else{
+                    return undefined
+				}
+            }
+          }
+        )
+        console.log('infinitquery',data)
+        console.log('typeof data',typeof data)
+
+        // 무한스크롤 구현
+        const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
+            console.log('isIntersecting',isIntersecting)
+            if(isIntersecting){
+                fetchNextPage();
+            }
+        };
+        // 스크롤 이벤트 타겟 지정
+        const { setTarget } = useIntersectionObserver({ onIntersect });
+
     return (
         <>
-            <Home data={data}/>
+            <Container>
+                    {status === "loading" && <div>loading...</div>}
+                    {status === "error" && <div>error</div>}
+                    {status === "success" &&
+                        data.pages.map((page, index) => 
+                            <ProductList key={index} completedProducts={false} data={page} setTarget={setTarget} />
+                    )}
+            </Container>
             <LinkFooter/>
         </>
     )
 };
 
-
 // 서버사이드 렌더링으로 카테고리의 쿼리를 보내서 상품리스트 가져옴
 export const getServerSideProps : GetServerSideProps = async ({query}) => {
     const {name} = query;
+    const categoryName = String(name);
+    const queryClient = new QueryClient()
+
     try{
-        const {data} = await getCategoryProductList(String(name));
-        return {
-            props : {
-                data
+        await queryClient.prefetchInfiniteQuery(
+            ['categoryList'],async()=>{
+              const res = await getCategoryProductList(categoryName)
+              return res.data;
             }
-        }
-    } catch(err){
-        console.log(err);
-        return {
-            props : {},
+          )
+        
+          return {
+              props: {
+                  // infiniteQuery를 사용하거나 Promise.all을 사용할 경우 JSON처리 필수
+                  dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+                  categoryName
+              },
+            }
+    } 
+    catch(error){
+        return{
+            notFound:true,
         }
     }
 }
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,16 +1,69 @@
-import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { dehydrate, QueryClient, useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { GetServerSideProps,  } from 'next';
 import React from 'react';
 import LinkFooter from '../../components/footer/LinkFooter';
 import Home from "../../components/home/Home";
 import { GetproductList } from '../../lib/api/product';
+import styled from 'styled-components';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
+import ProductList from '../../components/home/ProductList';
 
 
-const home = ({productList}:any) => {
+const Container = styled.div`
+    padding:0px 20px;
+    padding-top:20px;
+`
+
+const home = () => {
+    const lastPageNumber=4
+    const {
+        data, // 💡 data.pages를 갖고 있는 배열
+        fetchNextPage, // 💡 다음 페이지를 불러오는 함수
+        hasNextPage, // 다음 페이지가 있는지 여부, Boolean
+        status, 
+    } = useInfiniteQuery(
+          ["productList"] 
+        , GetproductList
+        , {
+            // 위의 fetch callback의 인자로 자동으로 pageParam을 전달.
+            getNextPageParam: (_lastPage,pages) => {
+                if(pages.length<lastPageNumber){
+                    return pages.length
+                }else{
+                    return undefined
+            // getNextPageParam 메서드가 falsy한 값을 반환하면 추가 fetch를 실행하지 않는다
+            //     }
+            //     // offset 값 받기 - 백엔드
+            //     // const { nextOffset, hasMore } = lastPage?.data;
+			// 	// if (!hasMore) return false;
+			// 	// else {
+			// 	// 	return Number(nextOffset);
+				}
+            }
+          }
+        )
+        console.log('infinitquery',data)
+
+        // 무한스크롤 구현
+        const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
+            console.log('isIntersecting',isIntersecting)
+            if(isIntersecting){
+                fetchNextPage();
+            }
+        };
+        const { setTarget } = useIntersectionObserver({ onIntersect });
+
     return (
         <>
-            <Home/>
+            <Container>
+                    {status === "loading" && <div>loading...</div>}
+                    {status === "error" && <div>error</div>}
+                    {status === "success" &&
+                        data.pages.map((page, index) => 
+                            <ProductList key={index} completedProducts={false} data={page} setTarget={setTarget} />
+                    )}
+            </Container>
             <LinkFooter/>
         </>
     )
