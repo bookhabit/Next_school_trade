0966e9037c5d4d6531d2623afa806f98f42eda01
bookhabit <qnrgoqlt@naver.com>
2023-03-29 15:40:20 +0900

    category 폴더변경

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import ElectronicIcon from "../public/static/svg/category/electronic.svg"
-import Category from '../components/category/Category';
-import LinkFooter from '../components/footer/LinkFooter';
+import Category from '../../components/category/Category';
+import LinkFooter from '../../components/footer/LinkFooter';
 
 const category = () => {
     return (
