399e46a7fce072343492c2eb3f4959644993f83e
baaami <lgh121546@naver.com>
2023-04-19 22:37:48 +0900

    로클 로그인, 회원가입 시 withCredentials 옵션 true로 request

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -5,7 +5,7 @@ import { SignUpAPIBody } from '../../types/auth';
 import { KakaoSignUpAPIBody } from '../../types/auth';
 
 // 로컬 회원가입 api
-export const signupAPI = (body:SignUpAPIBody)=>axios.post<LoggedUserType>("http://localhost:4000/auth/local/signup",body)
+export const signupAPI = (body:SignUpAPIBody)=>axios.post<LoggedUserType>("http://localhost:4000/auth/local/signup",body, {withCredentials: true})
 
 // 카카오 회원가입 api
 export const kakaoSignupAPI = (body:KakaoSignUpAPIBody)=>
@@ -17,7 +17,7 @@ axios.post<LoggedUserType>("http://localhost:4000/auth/kakao/signup",
 
 // 로그인 api - <LoggedUserType>
 export const loginAPI =  async (body:LoginFormValues)=>
-    await axios.post("http://localhost:4000/auth/local/signin",body)
+    await axios.post("http://localhost:4000/auth/local/signin",body, {withCredentials: true})
 
 // 로그아웃 api
 export const logoutAPI = ()=> axios.delete("/api/auth/logout")
