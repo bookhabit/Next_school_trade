93585968fbabe7b1476b3635ee93b7426f0eacdb
bookhabit <qnrgoqlt@naver.com>
2023-05-03 13:17:07 +0900

    프로필 수정 api연동

--- components/myPage/Profile.tsx
+++ components/myPage/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../styles/palette';
 import ProfileUserIcon from "../../public/static/svg/myPage/ProfileUserIcon.svg"
@@ -7,9 +7,11 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import Button from '../common/Button';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 
-const Container = styled.div`
+const Container = styled.form`
     padding:50px 20px;
     display:flex;
     flex-direction:column;
@@ -19,13 +21,13 @@ const Container = styled.div`
         margin-top:20px;
         position: relative;
         img{
-            background-color:#FEFAFA;
             padding:10px;
-            width:100px;
-            height:100px;
+            width:120px;
+            height:120px;
             border-radius:50px;
         }
         label{
+            cursor: pointer;
             background-image:url("/static/svg/myPage/profileCamera.svg");
             background-repeat:no-repeat;
             background-size: 50px 50px;
@@ -43,21 +45,30 @@ const Container = styled.div`
     .profile-input{
         margin-top:40px;
         text-align:center;
-        input{
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        .change-password-button{
             margin-top:40px;
-            padding-left:20px;
-            width:280px;
-            height:40px;
-            background-color:#F5F2F2;
-            border-radius:40px;
-        }
-        button{
+            padding-top:13px;
             width:280px;
             height:40px;
             background-color:#F5F2F2;
             border-radius:40px;
             font-size:15px;
-            font-weight:bold;
+            cursor: pointer;
+            &:hover{
+                background-color:${palette.gray_bb};
+            }
+        }
+        input{
+            width:280px;
+            height:40px;
+            margin-top:40px;
+            padding-left:20px;
+            background-color:#F5F2F2;
+            border-radius:40px;
         }
     }
 
@@ -69,34 +80,106 @@ const Container = styled.div`
 `
 
 const Profile = () => {
+    // 로그인 유저 정보 불러오기
+    const LoggedUser = useSelector((state:RootState)=>state.user)
+
+    // state
+    // 유저의 기본이미지 만들면 LoggedUser.profileImage 경로바꾸기 초기값
+    const [thumnail,setThumnail] = useState("/static/svg/myPage/ProfileUserIcon.svg")
+    const [profileImg,setProfileImg] = useState("/static/svg/myPage/ProfileUserIcon.svg")
+    const [nickname,setNickname] = useState<string>(LoggedUser.nickname)
+    const [password,setPassword] = useState<string>("")
+    const [confirmPassword,setConfirmPassword] = useState("")
+
+    const onUpload = (e: any) => {
+        const file = e.target.files[0];
+        setProfileImg(e.target.files[0])
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+      
+        return new Promise<void>((resolve) => {
+          reader.onload = () => {
+            const imageUrl = reader.result;
+            if (typeof imageUrl === 'string') {
+                setThumnail(imageUrl);
+            } else {
+                setThumnail("");
+            }
+            resolve();
+          };
+        });
+      };
+
+    // 비밀번호변경 모달
     const [changePassword,setChangePassword] = useState(false);
 
     const showPasswordInput = ()=>{
         setChangePassword(!changePassword);
     }
 
-    // 유저 정보 불러오기
-    const LoggedUser = useSelector((state:RootState)=>state.user)
+    // 프로필 수정 api
+    const onSubmit = async (event:React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+        // form 확인
+        const formData: FormData = new FormData();
+        formData.append('images',profileImg);
+        formData.append('nickname',nickname);
+        formData.append('password',password);
+        // FormData 객체의 내용 출력
+        for (const pair of formData.entries()) {
+            console.log(pair[0], pair[1]);
+        }
+        // api요청
+        try{
+            const result = await axios.post("http://localhost:4000/user/profile",
+            formData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                 },
+                 withCredentials: true,
+            })
+            console.log(result)
+            // api 성공 시 페이지 새로고침
+            const router= useRouter();
+            router.push("/user")
+        }catch(e){
+            console.log(e)
+        }
+
+    }
+
+    // 로그인된 유저정보를 업데이트하기 위해 useEffect실행
+    useEffect(()=>{
+
+    },[])
 
     return (
-        <Container>
+        <Container onSubmit={onSubmit}>
             <div className='profile-image'>
-                {/* <img src={LoggedUser.profileImage} alt="프로필 이미지"/> */}
-                <ProfileUserIcon/>
+                <img src={thumnail} alt="프로필 이미지"/>
                 <label htmlFor='file-input'/>
-                <input type="file" id="file-input"/>
+                <input type="file" id="file-input"   onChange={e => onUpload(e)}
+                />
             </div>
             <div className='profile-input'>
                 <input type='text' placeholder='닉네임'
-                defaultValue={LoggedUser.nickname}
+                value={nickname}
+                onChange={(event)=>setNickname(event.target.value)}
                 />
-                <button onClick={showPasswordInput} className='profile-input'>
+                <div onClick={showPasswordInput} className='change-password-button'>
                     비밀번호 변경
-                </button>
+                </div>
                 { changePassword ? 
                 <>
-                    <input type='text' placeholder='현재 비밀번호'/>
-                    <input type='text' placeholder='새 비밀번호'/>
+                    <input type='text' placeholder='현재 비밀번호' 
+                    value={password}
+                    onChange={(event)=>setPassword(event.target.value)}
+                    />
+                    <input type='text' placeholder='새 비밀번호'
+                    value={confirmPassword}
+                    onChange={(event)=>setConfirmPassword(event.target.value)}
+                    />
                 </> : null}
                 
             </div>
@@ -104,6 +187,7 @@ const Profile = () => {
                 <Button width='280px' height='40px' radius='30px'>수정하기</Button>
             </div>
         </Container>
+        
     );
 };
 
